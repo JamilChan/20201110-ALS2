e322038d7fa793aeb82145328c7bde2b0e4392f3
Dean Marco Dalager Birch Nielsen <1thagok1@gmail.com>
2020-11-17 11:36:47 +0100

    UserStory3 Branch created

---
+++ ALS2xUnitTest/CourseTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALS2xUnitTest {
+    class CourseTest {
+    public void CanChangeProductName() {
+      // Arrange
+      // Act
+      //Assert
+    }
+  }
+}
