93034037687ae109262cf39b3e3d3efe2a639511
Mathias Jensen <math23k9@eamv.dk>
2020-11-17 11:41:32 +0100

    nothing

---
+++ 20201110-ALS2/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _20201110_ALS2.Controllers {
+  public class AdminController : Controller {
+    public IActionResult Index() {
+      return View();
+    }
+  }
+}
---
+++ 20201110-ALS2/Models/IEducatorRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _20201110_ALS2.Models {
+  public interface IEducatorRepository
+  {
+    Educator Get(int educatorId);
+    void Add(Educator educator);
+    Educator Update(Educator educatorChanges);
+    Educator Delete()
+  }
+}
