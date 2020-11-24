4ead83d6ab65e93553e2dd1893f285f3fe67aba9
Simon Markussen <simonmarkussen92@gmail.com>
2020-11-24 14:37:44 +0100

    Charts Testing

---
+++ 20201110-ALS2/Controllers/ChartsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _20201110_ALS2.Models;
+
+namespace _20201110_ALS2.Controllers {
+  public class ChartsController : Controller {
+
+    //public IActionResult Index() {
+    //  return View("Index");
+    //}
+
+    // GET: /<controller>/  
+    public IActionResult Index() {
+      return View();
+    }
+
+    [HttpGet]
+    public JsonResult PopulationChart() {
+      var populationList = PopulationDataAccessaLayer.GetUsStatePopulationList();
+      return Json(populationList);
+    }
+  }
+} 
---
+++ 20201110-ALS2/Models/Chart.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _20201110_ALS2.Models {
+  public class Chart {
+    public object[] cols { get; set; }
+    public object[] rows { get; set; }
+  }
+}
