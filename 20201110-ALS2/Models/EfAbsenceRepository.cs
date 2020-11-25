b1eab20fdf76f67797778f9988b5f9f8910e25b6
Mathias Jensen <math23k9@eamv.dk>
2020-11-25 14:18:26 +0100

    Revert Revert Revert Please virk

--- 20201110-ALS2/Models/EfAbsenceRepository.cs
+++ 20201110-ALS2/Models/EfAbsenceRepository.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 
 namespace _20201110_ALS2.Models {
   public class EfAbsenceRepository : IAbsenceRepository {
