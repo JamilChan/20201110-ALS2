f820aa3460ee0e7485352395d50d097360eafacf
Mathias Jensen <math23k9@eamv.dk>
2020-11-25 13:49:39 +0100

    AbsenceByCourse

--- 20201110-ALS2/Models/IAbsenceRepository.cs
+++ 20201110-ALS2/Models/IAbsenceRepository.cs
@@ -14,5 +14,7 @@ namespace _20201110_ALS2.Models {
     Dictionary<Course, bool> CourseHasAbsence(List<Course> courseList, DateTime date);
     IQueryable<Absence> AbsencesForDateCourse(Course course, DateTime date);
 
+    List<Absence> AbsenceByCourse(Course course);
+
   }
 }
